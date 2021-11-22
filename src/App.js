6b092ff8d4a7b8c36a0f58a5e07e3fa4a60d0afa
pover0236 <pover0236@cox.net>
2021-11-22 10:55:08 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
+import Navbar from './components/navbar/navbar.component';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <header className="App-header">
+
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +20,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header>      
     </div>
   );
 }
---
+++ src/components/navbar/navbar.component.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+//import './navbar.styles.scss';
+
+const Navbar = () => (
+    <div>Navbar goes here</div>
+);
+
+export default Navbar;
