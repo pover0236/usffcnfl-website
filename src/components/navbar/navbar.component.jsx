f954d03b3d53bec182427457f61539a502236bec
pover0236 <pover0236@cox.net>
2021-11-23 09:57:48 -0500

    Added dropdown links to top-level navbar items

--- src/components/navbar/navbar.component.jsx
+++ src/components/navbar/navbar.component.jsx
@@ -4,9 +4,21 @@ import './navbar.styles.css';
 const Navbar = () => (
 <div id='navmenu' class='navbar'>
 	<ul>
-		<li><a href='#about'>About</a></li>
-		<li><a href='#training'>Seminars and Training</a></li>
-		<li><a href='#risk'>Risk Management Forms</a></li>
+		<li><a href='#'>About</a></li>
+		<li class='dropdown'>
+			<a href='javascript:void(0)' class='dropbtn'>Seminars and Training</a>
+			<div class="dropdown-content">
+					<a href="#">Seminar Schedule</a>
+					<a href="#">Unscheduled Training</a>
+    			</div>			
+		</li>
+		<li class='dropdown'>
+			<a href='javascript:void(0)' class='dropbtn'>Risk Management Forms</a>
+				<div class="dropdown-content">
+					<a href="#">Open Risk Forms</a>
+					<a href="#">Closed Risk Forms</a>
+    			</div>
+		</li>
 	</ul>
 </div>
 );
