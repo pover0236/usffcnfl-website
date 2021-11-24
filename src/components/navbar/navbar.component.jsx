2478d4e2bfa7eacaab31d2babc2cb6819e74f9d0
pover0236 <pover0236@cox.net>
2021-11-24 13:11:44 -0500

    Implemented responsive design on navigation bar

--- src/components/navbar/navbar.component.jsx
+++ src/components/navbar/navbar.component.jsx
@@ -2,24 +2,50 @@ import React from 'react';
 import './navbar.styles.css';
 
 const Navbar = () => (
-<div id='navmenu' class='navbar'>
-	<ul>
-		<li><a href='#'>About</a></li>
-		<li class='dropdown'>
-			<a href='javascript:void(0)' class='dropbtn'>Seminars and Training</a>
-			<div class="dropdown-content">
-					<a href="#">Seminar Schedule</a>
-					<a href="#">Unscheduled Training</a>
-    			</div>			
-		</li>
-		<li class='dropdown'>
-			<a href='javascript:void(0)' class='dropbtn'>Risk Management Forms</a>
-				<div class="dropdown-content">
-					<a href="#">Open Risk Forms</a>
-					<a href="#">Closed Risk Forms</a>
-    			</div>
-		</li>
-	</ul>
+<div className='navbar'>
+	<div className='horiznav'>
+		<ul>
+			<li><a href='about#'>About</a></li>
+			<li className='dropdown'>
+				<a href='#semandtng'>Seminars and Training</a>
+				<div className="dropdown-content">
+						<a href="#schedule">Seminar Schedule</a>
+						<a href="#unscheduled">Unscheduled Training</a>
+				</div>			
+			</li>
+			<li className='dropdown'>
+				<a href='#riskfms'>Risk Management Forms</a>
+					<div className="dropdown-content">
+						<a href="#openrisks">Open Risk Forms</a>
+						<a href="#closedrisks">Closed Risk Forms</a>
+					</div>
+			</li>
+		</ul>
+	</div>
+	<div className='vertnav'>
+		<div className='menu'>
+			<a href='#menu-items'>
+				<img src={require('./menu.png')} width='25px' alt='menu' />
+			</a>
+			<ul className='menu-items'>
+				<li><a href='#about'>About</a></li>
+				<li className='dropdown'>
+					<a href='#semandtng'>Seminars and Training</a>
+					<div className="dropdown-content">
+							<a href="#schedule">Seminar Schedule</a>
+							<a href="#unscheduled">Unscheduled Training</a>
+					</div>
+				</li>	
+				<li className='dropdown'>
+					<a href='#riskfms'>Risk Management Forms</a>
+						<div className="dropdown-content">
+							<a href="#openrisks">Open Risk Forms</a>
+							<a href="#closedrisks">Closed Risk Forms</a>
+						</div>
+				</li>
+			</ul>
+		</div>
+	</div>
 </div>
 );
 
